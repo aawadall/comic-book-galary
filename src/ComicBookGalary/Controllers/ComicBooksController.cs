7d53b085e458fb0b14debefa3aa1a3011d654f12
Ahmed Awadallah <aawadall@ualberta.ca>
2017-08-05 15:34:53 -0400

    Updated comic book detail view

--- src/ComicBookGalary/Controllers/ComicBooksController.cs
+++ src/ComicBookGalary/Controllers/ComicBooksController.cs
@@ -10,7 +10,18 @@ namespace ComicBookGalary.Controllers
     {
         public ActionResult Detail()
         {
-            
+            ViewBag.SeriesTitle = "Gone with the wind";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>some details about the issue</p><p>it is not that important</p>";
+            ViewBag.Artists = new string[]
+            {
+                "Artist 1",
+                "Artist 2",
+                "Artist 3",
+                "Artist 4",
+                "Artist 5",
+                "Artist 6"
+            };
             return View();
         }
     }
