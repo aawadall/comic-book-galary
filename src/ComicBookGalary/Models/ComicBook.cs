eef730f39e2662360cf225bb7a66595473231e8d
Ahmed Awadallah <aawadall@ualberta.ca>
2017-08-06 12:33:21 -0400

    using new layout

--- src/ComicBookGalary/Models/ComicBook.cs
+++ src/ComicBookGalary/Models/ComicBook.cs
@@ -12,7 +12,7 @@ namespace ComicBookGalary.Models
         public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
-        public bool Favortte { get; set; }
+        public bool Favorite { get; set; }
 
         public string DisplayText => SeriesTitle + " # " + IssueNumber;
 
